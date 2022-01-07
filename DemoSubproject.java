9e97028a98954f227cf2fa7ffbc2f5ce14fffb56
rajnikant <59799061+automation555@users.noreply.github.com>
2022-01-07 18:01:57 +0530

    Update DemoSubproject.java

--- DemoSubproject.java
+++ DemoSubproject.java
@@ -1,6 +1,13 @@
 
-public class DemoSubproject {
+public class DemoSubproject implements Cloneable{
 	public static void main(String[] args) {
 		System.out.println("");
 	}
+	
+	
+public Object clone(String accountnumber) throws  //Flaw :cloneable() method that is not declared final
+CloneNotSupportedException
+{
+Object returnMe = new BankAccount(account number);
+}
 }
